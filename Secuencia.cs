87eceda962f3dc5fc9c809d79a77aa5a97c9bf4a
Ethan <ethan-rivas@hotmail.com>
2020-09-30 00:15:32 -0500

    Agregar archivos de proyecto.

---
+++ Secuencia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Secuencia
+    {
+        public string Calculate(int limite)
+        {
+            ulong pivot;
+            ulong a = 0;
+            ulong b = 1;
+
+            string secuencia = "";
+
+            for (int i = 1; i <= limite; i++)
+            {
+                pivot = a;
+                a = b;
+                b = pivot + a;
+
+                if (i < limite)
+                    secuencia += pivot.ToString() + ", ";
+                else
+                    secuencia += pivot.ToString() + ".";
+            }
+
+            return secuencia;
+        }
+    }
+}
